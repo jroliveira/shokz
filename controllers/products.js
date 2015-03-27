d0b81262a8fa48208dba2fdb953c6c30fd710138
Luiz Freneda <lfreneda@gmail.com>
2015-03-26 23:48:28 -0300

    Naming

--- controllers/products.js
+++ controllers/products.js
@@ -9,13 +9,13 @@ var wrap = require('co-monk');
 var db = monk('mongodb://admin:admin@dharma.mongohq.com:10038/products');
 
 // Wrap monk in generator goodness
-var products = wrap(db.get('products'));
+var productsCollection = wrap(db.get('products'));
 
 var render = views(__dirname + '/../views', {
     map: { html: 'swig' }
 });
 
 module.exports.home = function *home() {
-    var productsList = yield products.find({});
-    this.body = yield render('products', { 'products': productsList });
+    var products = yield productsCollection.find({});
+    this.body = yield render('products', { 'products': products });
 };
