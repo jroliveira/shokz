ab9a68da8e5e654b4500bee88ea427ac198e16e4
Junior Oliveira <junolive@gmail.com>
2015-03-27 01:31:44 -0300

    Broken tests

--- test/routeSpec.js
+++ test/routeSpec.js
@@ -1,18 +1,35 @@
 /*global describe, it*/
 'use strict';
+
 var superagent = require('supertest');
 var app = require('../app');
 
 function request() {
-	return superagent(app.listen());
+    return superagent(app.listen());
 }
 
 describe('Routes', function () {
-  describe('GET /', function () {
-    it('should return 200', function (done) {
-      request()
-        .get('/')
-        .expect(200, done);
+    describe('GET /products/1', function () {
+        it('should return 200', function (done) {
+            request()
+                .get('/products/1')
+                .expect(200, done);
+        });
+    });  
+    
+    describe('GET /product/turtle-shell-racers', function () {
+        it('should return 200', function (done) {
+            request()
+                .get('/product/turtle-shell-racers')
+                .expect(200, done);
+        });
     });
-  });  
+    
+    describe('GET /product/notfound', function () {
+        it('should return 404', function (done) {
+            request()
+                .get('/product/notfound')
+                .expect(404, done);
+        });
+  });
 });
