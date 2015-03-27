2c1e14391b26c0273c18ba0e1f49b02c3f6d6270
Luiz Freneda <lfreneda@gmail.com>
2015-03-26 23:46:52 -0300

    getting products from mongodb

--- controllers/products.js
+++ controllers/products.js
@@ -2,20 +2,20 @@
 
 var views = require('co-views');
 var parse = require('co-body');
-    
-var products = [
-    { id: 1, name: 'Game Of Thrones 3D Map Puzzle', price: 43.72, img: 'images/game-of-thrones-cityscape-3d-puzzle.jpg' },
-    { id: 2, name: 'Super Mario Bros Chess Board', price: 34.99, img: 'images/super-mario-chess-board.jpg' },
-    { id: 3, name: 'Archeress Leather Quiver', price: 130.00, img: 'images/archeress-leather-quiver.jpg' },
-    { id: 4, name: 'World Of Warcraft Horde Armory Chest', price: 550.00, img: 'images/warcraft-horde-chest.jpg' },
-    { id: 5, name: 'Turtle Shell Racers', price: 245.10, img: 'images/turtle-shell-racers.jpg' }
-    
-];
+
+// Set up monk
+var monk = require('monk');
+var wrap = require('co-monk');
+var db = monk('mongodb://admin:admin@dharma.mongohq.com:10038/products');
+
+// Wrap monk in generator goodness
+var products = wrap(db.get('products'));
 
 var render = views(__dirname + '/../views', {
     map: { html: 'swig' }
 });
 
 module.exports.home = function *home() {
-    this.body = yield render('products', { 'products': products });
+    var productsList = yield products.find({});
+    this.body = yield render('products', { 'products': productsList });
 };
